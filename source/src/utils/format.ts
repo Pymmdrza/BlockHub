a5dcf10711ec064d4f937efecc805002d4563ec4
MMDRZA <info@mmdrza.com>
2025-01-29 02:11:59 +0300

    Delete source directory

--- source/src/utils/format.ts
+++
@@ -1,5 +0,0 @@
-export const truncateAddress = (address: string, maxLength: number = 24): string => {
-  if (!address) return '';
-  if (address.length <= maxLength) return address;
-  return `${address.slice(0, maxLength)}...`;
-};
