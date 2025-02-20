84369c0d69e521db8563961683e707f22e9f5cce
MMDRZA <info@mmdrza.com>
2025-02-20 04:30:38 +0300

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute environment variables in the Nginx configuration template
-envsubst '$DOMAIN' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Start Nginx in the foreground
-exec nginx -g 'daemon off;'
